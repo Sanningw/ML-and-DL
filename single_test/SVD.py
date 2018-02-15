fea7cfb24ddf9ca6b6132305a2986d4781fc10f8
leeweizhe1993 <32242263+leeweizhe1993@users.noreply.github.com>
2018-02-15 16:59:24 +0800

    Image_SVD and stock_MA

--- single_test/SVD.py
+++ single_test/SVD.py
@@ -14,7 +14,7 @@ from pprint import pprint
 
 '''
 参数说明
-u：特征值， v：左特征向量，K：右特征向量
+sigma：特征值， u：左特征向量，v：右特征向，K：选取的奇异值个数
 '''
 
 def restore1(sigma, u, v, K):
@@ -22,12 +22,13 @@ def restore1(sigma, u, v, K):
     n = len(v[0])
     a = np.zeros((m, n))
     for k in range(K + 1):
-        uk = u[:, k].reshape(m, 1)
-        vk = v[k].reshape(1, n)
-        a += sigma[k] * np.dot(uk, vk)
-    a[a < 0] = 0
-    a[a > 255] = 255
-    return np.rint(a).astype('uint8')
+        uk = u[:, k].reshape(m, 1)        #取第k列
+        vk = v[k].reshape(1, n)           #取第k列
+        a += sigma[k] * np.dot(uk, vk)    #取前k个奇异值数乘dot(u, v)
+    #a[a < 0] = 0
+    #a[a > 255] = 255
+    a = a.clip(0, 255)
+    return np.rint(a).astype('uint8')    #四舍五入，转换成整数
 
 def restore2(sigma, u, v, K):
     m = len(u)
@@ -35,33 +36,38 @@ def restore2(sigma, u, v, K):
     a = np.zeros((m, n))
     for k in range(K+1):
         for i in range(m):
-            a[i] += sigma[k] * u[i][k] * v[k]
+            a[i] += sigma[k] * u[i][k] * v
     a[a < 0] = 0
     a[a > 255] = 255
     return np.rint(a).astype('uint8')
 
 if __name__ == "__main__":
     A = Image.open("D://数据集//小象学院//5.Package//lena.png", 'r')
-    print(A)
-    output_path = r'.\SVD_Output'
+    print(A)    #命名文件为A
+    output_path = r'D://数据集//小象学院//5.Package//SVD_Output'
     if not os.path.exists(output_path):
         os.mkdir(output_path)
-    a = np.array(A)
+    a = np.array(A)    #转为numpy格式
     print(a.shape)
+    
     K = 50
+    '''对RGB三个通道分别计算SVD'''
     u_r, sigma_r, v_r = np.linalg.svd(a[:, :, 0])
     u_g, sigma_g, v_g = np.linalg.svd(a[:, :, 1])
-    u_b, sigma_b, v_b = np.linalg.svd(a[:, :, 2])
-    plt.figure(figsize=(11, 9), facecolor='w')
+    u_b, sigma_b, v_b = np.linalg.svd(a[:, :, 2]) 
+    plt.figure(figsize = (11, 9), facecolor = 'w')
+    
     mpl.rcParams['font.sans-serif'] = ['simHei']
     mpl.rcParams['axes.unicode_minus'] = False
-    for k in range(1, K+1):
+    
+    for k in range(1, K + 1):
         print(k)
         R = restore1(sigma_r, u_r, v_r, k)
         G = restore1(sigma_g, u_g, v_g, k)
         B = restore1(sigma_b, u_b, v_b, k)
-        I = np.stack((R, G, B), axis=2)
+        I = np.stack((R, G, B), axis = 2)
         Image.fromarray(I).save('%s\\svd_%d.png' % (output_path, k))
+        
         if k <= 12:
             plt.subplot(3, 4, k)
             plt.imshow(I)
---
+++ single_test/fuzzywuzzy_trail.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 15:18:31 2018
+
+@author: aa
+"""
+
+import numpy as np
+import pandas as pd
+from fuzzywuzzy import fuzz, process

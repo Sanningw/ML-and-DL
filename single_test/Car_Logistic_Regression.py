775eca4e1c8130d1d821a0d945f884118904a406
leeweizhe1993 <32242263+leeweizhe1993@users.noreply.github.com>
2018-02-23 00:50:33 +0800

    One_hot & ROC

--- single_test/Car_Logistic_Regression.py
+++ single_test/Car_Logistic_Regression.py
@@ -33,21 +33,45 @@ if __name__ == '__main__':
         x = pd.concat((x, t), axis = 1)    #横向合并
     
     y = pd.Categorical(data['accept']).codes    #对'accept'列元素进行汇总，用数字代替
-    y[y == 1] = 0
-    y[y >= 2] = 1
-    
+
     x, x_test, y, y_test = train_test_split(x, y, test_size = 0.3)    #分割训练/测试集
     #使用交叉验证进行对数回归，惩罚项由lg(10^-3)~lg(10^4), 5折交叉
     clf = LogisticRegressionCV(Cs = np.logspace(-3, 4, 8), cv = 5)
     clf.fit(x, y)
     
     y_hat = clf.predict(x)
-    print('训练集精度：', metrics.accuracy_score(y, y_hat))
+    print('训练集精度：', metrics.accuracy_score(y, y_hat))    #返回精度(1-错误率)
     y_test_hat = clf.predict(x_test)
     print('测试集精度：', metrics.accuracy_score(y_test, y_test_hat))
     n_class = len(np.unique(y))
     
+    #ROC
     if n_class > 2:
-        y_test_one_hot = label_binarize(y_test, classes = np.arange(n_class))
-    
-    
+        y_test_one_hot = label_binarize(y_test, classes = np.arange(n_class))    #利用二值化函数将y_test进行one-hot编码
+        y_test_one_hot_hat = clf.predict_proba(x_test)    # 返回属于各个标签概率的预测值
+        fpr, tpr, _ = metrics.roc_curve(y_test_one_hot.ravel(), y_test_one_hot_hat.ravel())    #ROC值
+        print('Micro AUC:\t', metrics.auc(fpr, tpr))
+        auc = metrics.roc_auc_score(y_test_one_hot, y_test_one_hot_hat, average = 'micro')
+        print('Micro AUC(System):\t', auc)
+        auc = metrics.roc_auc_score(y_test_one_hot, y_test_one_hot_hat, average = 'macro')
+        print('Macro AUC:\t', auc)
+    else:
+        fpr, tpr, _ = metrics.roc_curve(y_test.ravel(), y_test_hat.ravel())
+        print('AUC:\t', metrics.auc(fpr, tpr))
+        auc = metrics.roc_auc_score(y_test, y_test_hat)
+        print('AUC(System):\t', auc)
+        
+    mpl.rcParams['font.sans-serif'] = 'SimHei'
+    mpl.rcParams['axes.unicode_minus'] = False
+    plt.figure(figsize = (8, 7), dpi = 80, facecolor = 'w')
+    plt.plot(fpr, tpr, 'r-', lw = 2, label = 'AUC=%.4f' % auc)
+    plt.legend(loc = 'best')
+    plt.xlim((-0.01, 1.02))
+    plt.ylim((-0.01, 1.02))
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.xlabel('False Positive Rate', fontsize = 14)
+    plt.ylabel('True Positive Rate', fontsize = 14)
+    plt.grid(b=True, ls=':')
+    plt.title('ROC曲线和AUC', fontsize=18)
+    plt.show()

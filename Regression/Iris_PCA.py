4fbebb8730a229b29b3b8fa5f96115f47ca5dc1b
785804871@qq.com <785804871@qq.com>
2018-02-24 21:51:45 +0800

    import packages

---
+++ Regression/Iris_PCA.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 21:42:11 2018
+
+@author: Li
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, SelectPercentile, chi2
